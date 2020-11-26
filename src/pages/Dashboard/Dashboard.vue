f6bc568b54af598c88fc4e0c8cfb8a983012969e
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-11-26 15:55:56 +0100

    add info

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -5,7 +5,46 @@
       <div>
         <card>
           <h5 slot="header" class="title">Dashboard</h5>
-
+          <div class="row">
+            <div class="col-md-6 pr-md-1">
+              <h5 class="card-category">{{ $t('dashboard.peerId') }}</h5>
+              <h3 class="card-title">
+                <i class="tim-icons icon-badge text-success"></i>
+                {{ identity == null ? '' : identity.data.peer_id }}
+              </h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 pr-md-1">
+              <h5 class="card-category">{{ $t('dashboard.enr') }}</h5>
+              <h4 class="card-title">
+                <i class="tim-icons icon-planet text-success"></i>
+                {{ identity == null ? '' : identity.data.enr }}
+              </h4>
+            </div>
+          </div>
+          <div v-if="showP2PAddresses">
+            <h4 class="title">P2P addresses</h4>
+            <div class="row" v-for="p2pAddress in p2pAddresses" :key="p2pAddress">
+              <div class="col-md-12 pr-md-1">
+                <h4 class="card-title">
+                  <i class="tim-icons icon-planet text-success"></i>
+                  {{ p2pAddress }}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div v-if="showDiscoveryAddresses">
+            <h4 class="title">Discovery addresses</h4>
+            <div class="row" v-for="address in discoveryAddresses" :key="address">
+              <div class="col-md-12 pr-md-1">
+                <h4 class="card-title">
+                  <i class="tim-icons icon-planet text-success"></i>
+                  {{ address }}
+                </h4>
+              </div>
+            </div>
+          </div>
         </card>
       </div>
       <router-view></router-view>
@@ -28,6 +67,30 @@ export default {
     }
   },
   computed: {
+    p2pAddresses() {
+      if (!this.showP2PAddresses) {
+        return [];
+      } else {
+        return this.identity.data.p2p_addresses;
+      }
+    },
+    showP2PAddresses() {
+      return this.identity != null &&
+        this.identity.data.hasOwnProperty('p2p_addresses') &&
+        Array.isArray(this.identity.data.p2p_addresses);
+    },
+    discoveryAddresses() {
+      if (!this.showDiscoveryAddresses) {
+        return [];
+      } else {
+        return this.identity.data.discovery_addresses;
+      }
+    },
+    showDiscoveryAddresses() {
+      return this.identity != null &&
+        this.identity.data.hasOwnProperty('discovery_addresses') &&
+        Array.isArray(this.identity.data.discovery_addresses);
+    },
     ...mapState([
       'services',
     ])
@@ -46,7 +109,7 @@ export default {
     async refreshData() {
       try {
         this.identity = await this.services.ethereumClient.nodeAPI.identity();
-        this.healthy =  await this.services.ethereumClient.nodeAPI.isHealthy();
+        this.healthy = await this.services.ethereumClient.nodeAPI.isHealthy();
       } catch (e) {
         this.$notifyMessage('danger', e);
       }
